576f5f4fd12372f113a89d5e76fd30bada553081
Felipe <fehlipecardooso@gmail.com>
2022-10-17 22:31:32 -0300

    vetor exercicio estudante

---
+++ VetorEstudante/Estudante.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace VetorEstudante
+{
+    public class Estudante
+    {
+        string nome { get; set; }
+        string email { get; set; }
+
+
+        public Estudante(string Nome, string Email)
+        {
+            nome = Nome;
+            email = Email;
+        }
+
+        public override string ToString()
+        {
+            return nome + ", " + email;
+        }
+    }
+}
---
+++ VetorEstudante/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace VetorEstudante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudante[] vect = new Estudante[10];
+
+            Console.WriteLine("Quanto quartos serão alugados");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i <= n; i++)
+            {
+                Console.WriteLine($"Número do quarto {i}");
+
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+
+                Console.Write("Quarto: ");
+                int quarto = Convert.ToInt16(Console.ReadLine());
+                vect[quarto] = new Estudante(nome, email);
+
+            }
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados:");
+            for (int i = 0; i < 10; i++)
+            {
+                if (vect[i] != null)
+                {
+                    Console.WriteLine(i + ": " + vect[i]);
+                }
+
+
+            }
+        }
+    }
+}
